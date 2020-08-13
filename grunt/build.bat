1e2ef0717f8477c7d5c9653578064e38713ecd4d
Nathan Harris <648059+Derjyn@users.noreply.github.com>
2020-08-12 18:10:18 -0700

    Cleanup

--- grunt/build.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-grunt
-
-PAUSE
--- grunt/setup.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-npm install --save-dev grunt grunt-contrib-concat grunt-contrib-copy grunt-contrib-cssmin grunt-contrib-imagemin grunt-contrib-uglify grunt-processhtml
-
-PAUSE
